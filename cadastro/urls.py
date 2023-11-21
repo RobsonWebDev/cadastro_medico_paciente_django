52cda26b19e1531fb98bb0e2a40c1ea5b21f7b4f
RobsonWebDev <robsonwebpro@gmail.com>
2023-11-21 20:56:13 -0300

    Algumas alterações no model e uma simples estilização nas pages

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from cadastro import views
 
+app_name = 'cadastro'
+
 urlpatterns = [
+    path("signup/", views.signup, name = 'signup'),
+    path("login/", views.login, name = 'login'),
     path("", views.index, name = 'index'),
 ]
--- cadastro/views/index.py
+++ cadastro/views/index.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 
 def index(request):
     return render(request, 'cadastro/index.html')
+
+def login(request):
+    return render(request, 'cadastro/login.html')
+
+def signup(request):
+    return render(request, 'cadastro/signup.html')
